77b94d96f50a77c799f67763df8ab43bc87d72c8
MMIX2009 <btshilombo@comcast.net>
2023-02-11 16:38:31 -0800

    Update app.py

--- app.py
+++ app.py
@@ -21,12 +21,12 @@ covid['Date'] = pd.to_datetime(covid['Date']).dt.strftime('%Y-%m-%d')
 country_options = covid['Country'].unique().tolist()
 st.write(covid)
 
-date-options = covid['Date'].unique().tolist()
+date_options = covid['Date'].unique().tolist()
 date = st.selectbox('Which date would you like to see?', date_options, 100)
 country = st.selectbox('Which country would you like to see?', country_options, ['Brazil')
                                                                                  
 covid = covid[covid['Country'].isin(country)]
-covid =covid[covid['Date']==date]
+covid = covid[covid['Date']==date]
 
 fig2 = px.bar(covid, x="Country", y="Confirmed, color="Country", range_y=[0,35000])
 fig2.update_layout(width=800)
