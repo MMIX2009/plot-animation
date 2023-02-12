269a880974633121af44c52886458ed879bdeb6f
MMIX2009 <btshilombo@comcast.net>
2023-02-11 16:05:49 -0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+df = px.data.gapminder()
+st.write(df)
+
+# gapmider GDP data Plot
+your-options = df['year'].unique().toList()
+year = st.slectbox('Which year do you want to see?' , year_options, 0)
+df = df[df['year']==year]
+
+fig = px.scatter()
+fig.update_layout(width=800)
+st.write(fig)
