c3c668d62c7068f1341066556f16567aafd7c65d
MMIX2009 <btshilombo@comcast.net>
2023-02-11 16:13:49 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ st.write(df)
 
 # gapmider GDP data Plot
 your_options = df['year'].unique().tolist()
-year = st.slectbox('Which year do you want to see?' , year_options, 0)
+year = st.selectbox('Which year do you want to see?' , year_options, 0)
 df = df[df['year']==year]
 
-fig = px.scatter()
+fig = px.scatter(df, x="gdpPrecap", y="LifeExp", size="pop", color="continent", hover_name="continent", log_x=True, size_max=55, range_x=[100,100000], range_y=[25,90])
 fig.update_layout(width=800)
 st.write(fig)
