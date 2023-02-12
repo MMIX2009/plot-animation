df1d2835c347bf66e72bbc4a96f6efdc3cbc961f
MMIX2009 <btshilombo@comcast.net>
2023-02-11 19:48:49 -0800

    Update drc-cities-pop.py

--- drc-cities-pop.py
+++ drc-cities-pop.py
@@ -28,7 +28,7 @@ fig.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = 5
 fig.update_layout(width=800)
 st.write(fig)
 
-fig2 = px.scatter(drc, x="Population", y="Growth-Rate", size="Population", color="City", hover_name="City", log_x=True, size_max=100, range_x=[2000,18000000],
+fig2 = px.scatter(drc, x="Population", y="Growth-Rate", size="Population", color="City", hover_name="City", log_x=True, size_max=100, range_x=[2000,18000000], range_y=[min_y,max_y],
                 animation_frame="Year", animation_group="City")
-fig2.update_layout(width=800, range_y=[min_y,max_y])
+fig2.update_layout(width=800)
 st.write(fig2)
